ee23015ece9dc14d4a036f2561e3cdbf7d01eb49
Adem Tiss <tissadem.2492006@gmail.com>
2026-05-17 18:23:08 +0100

    Learning Arrow functions

---
+++ script.js
@@ -0,0 +1,8 @@
+const a = n=>  n*n
+const divide =(a,b) => {
+    console.log("This is a division function")
+    return a/b
+}
+console.log(divide(9,3));
+const isAdult = age => age >=18 
+console.log(isAdult(30))
