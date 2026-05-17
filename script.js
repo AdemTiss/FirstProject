f937a8c5d74492ebaecc2825b04233d5a6563a0e
Adem Tiss <tissadem.2492006@gmail.com>
2026-05-17 18:52:12 +0100

    Learned to use arrow functions

--- script.js
+++ script.js
@@ -6,3 +6,29 @@ const divide =(a,b) => {
 console.log(divide(9,3));
 const isAdult = age => age >=18 
 console.log(isAdult(30))
+
+
+const applyAndLog=(func,n)=>{
+        console.log("Input : " , n , ", result : ", func(n))
+
+
+
+}
+
+
+const square = n => n*n 
+
+applyAndLog(square,5)
+
+
+const applyALL = (n , T) =>{
+
+    for (let  i = 0 ; i <T.length ; i++ ){
+            console.log(T[i].name,"(",n,")=",T[i](n))
+
+
+    }
+
+
+}
+applyALL(4,[isAdult,square])
