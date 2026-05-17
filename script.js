d8e7e57280efa18331948688aa7ad67dfe8fb2d5
Adem Tiss <tissadem.2492006@gmail.com>
2026-05-17 20:17:47 +0100

    Removed the Chapter 1 old exercices and did new ones

--- script.js
+++ script.js
@@ -1,34 +1,65 @@
-const a = n=>  n*n
-
-const isAdult = age => age >=18 
-console.log(isAdult(30))
-
-
-const applyAndLog=(func,n)=>{
-        console.log("Input : " , n , ", result : ", func(n))
+const TransformTemp= a => (a*(9/5)) + 32
+console.log(TransformTemp(24))
+const ReverseString = ch  => ch.split("").reverse().join("")
+console.log(ReverseString("Hello"))
+const LongerString = (ch1,ch2) => ch1.length>=ch2.length ? ch1:  ch2 
+console.log(LongerString("adem","HHHHH"))
 
 
+const fizzBuzz = n => n%3==0 ? "Fizz" : n%5==0 ? "FizzBuzz" : n 
 
-}
+console.log(fizzBuzz(11))
 
-
-const square = n => n*n 
-
-applyAndLog(square,5)
-
-
-const Calculator=(a,b,T)=>{
-    for ( let i = 0 ; i< T.length ; i++){
-        T[i](a,b)
+const maxNum = L => {
+    let max = L[0]
+    for( let i = 1 ; i <L.length ; i++){
+        if (L[i] > max) {
+            max = L[i];
+        }
 
     }
+    return max 
+}
+console.log(maxNum([1,2,3,4]))
+
+
+const numVowels = ch => {
+    let VowelsTotalOccurence = 0 
+    for ( let i = 0 ; i < ch.length ; i++){
+            if("aeoyi".includes(ch[i])){
+                VowelsTotalOccurence+=1
+            }
+    }
+    return VowelsTotalOccurence 
+
 }
 
-const add =(a,b)=> console.log(a,"+",b,"=",a+b) 
-const substract =(a,b)=> console.log(a,"-",b,"=",a-b) 
-const multiply=(a,b)=> console.log(a,"*",b,"=",a*b) 
-const divide =(a,b)=> console.log(a,"/",b,"=",a/b) 
+console.log(numVowels("adem"))
+
+const noDuplicate = L => { 
+    let L1=[]
+    for ( let i = 0 ; i < L.length ; i++){
+        // this one is not working and ikd why 
+        if (!(L1.includes(L[i]))){
+            L1.push(L[i])
+        }
+    
+
+    }
+    return L1
+}
+
+console.log(noDuplicate([1,2,2,3,4,1,3,2,4]))
 
 
+const repeat= (func , n )=>{
 
-Calculator(2,4,[add,substract,multiply,divide])
+    for (let i = 0 ; i< n ; i++){
+        func()
+    }
+
+}
+
+const compose = (func1,func2)=> n =>func2(func1(n))
+
+const func1ThenFunc2 =  compose(func1,func2)
